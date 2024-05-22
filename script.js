aaee2209afa1eae7fdfae4be6d5d4ca0b06d609a
Santiago Palomares <spaloma.v1@gmail.com>
2024-05-21 19:16:11 -0700

    now much better on mobile

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
+  const hamburgerIcon = document.querySelector('.hamburger-icon');
+  const menuLinks = document.querySelector('.menu-links');
+  hamburgerIcon.classList.toggle('open');
+  menuLinks.classList.toggle('open');
+}
