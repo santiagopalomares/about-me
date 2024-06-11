8893c748ab0d079f17b407a4bae60e12b44caa11
Santiago Palomares <spaloma.v1@gmail.com>
2024-06-10 18:21:14 -0700

    using template

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function toggleMenu() {
-  const hamburgerIcon = document.querySelector('.hamburger-icon');
-  const menuLinks = document.querySelector('.menu-links');
-  hamburgerIcon.classList.toggle('open');
-  menuLinks.classList.toggle('open');
-}
